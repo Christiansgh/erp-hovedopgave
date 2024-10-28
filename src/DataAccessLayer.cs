53c7a1bed12bf4c20a02b80eeac83cd586e61981
Christian Arensbak <christian.arensbak@twoday.com>
2024-10-28 13:45:11 +0100

    feat: Added dataaccesslayer.

---
+++ src/DataAccessLayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace erp.DataAccess;
+
+public static class DataAccessLayer { //TODO: Consider using env var or user secret instead.
+  private static readonly string _connectionString = "Data Source=37.27.179.21\\SQLEXPRESS22,1433;database=ERP;user id=sa;password='itsteatime-123';TrustServerCertificate=True";
+  //TODO: Exception handling
+  public static async Task<SqlDataReader> ExecuteQuery(string query) {
+    var con = new SqlConnection(_connectionString);
+    con.Open();
+    var command = new SqlCommand(query, con);
+    SqlDataReader result = await command.ExecuteReaderAsync();
+    return result;
+  }
+}
