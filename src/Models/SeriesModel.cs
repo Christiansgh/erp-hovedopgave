f77048514778e162580ebd7045275f6141c716d3
Christian Arensbak <christian.arensbak@twoday.com>
2024-11-05 15:03:16 +0100

    fix: Updated model class to match the decimal format for price in the database.

--- src/Models/SeriesModel.cs
+++ src/Models/SeriesModel.cs
@@ -3,6 +3,6 @@
 namespace erp.Models;
 
 [JsonSerializable(typeof(SeriesModel))]
-public record SeriesModel(string Name, string Brand, int Price, List<SizeModel> Sizes);
+public record SeriesModel(string Name, string Brand, decimal Price, List<SizeModel> Sizes, int Id);
 [JsonSerializable(typeof(SizeModel))]
 public record SizeModel(string SKU, int Size, int Stock);
